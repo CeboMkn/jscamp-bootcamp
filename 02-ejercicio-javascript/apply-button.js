811c784ecbbf1adeb67b16d6ee9a04b7a1902101
CeboMkn <195233514+CeboMkn@users.noreply.github.com>
2025-11-19 17:08:04 +0100

    Primeras funcionalidades

--- 02-ejercicio-javascript/apply-button.js
+++ 02-ejercicio-javascript/apply-button.js
@@ -1,1 +1,11 @@
 /* Aquí va la lógica para dar funcionalidad al botón de "Aplicar" */
+const contain_btn = document.querySelector('.btn_active_contain')
+
+contain_btn?.addEventListener('click', (e) => {
+    const element = e.target
+    if (element.classList.contains('btn_active')) {
+        element.textContent = '¡Aplicado!'
+        element.disabled = true
+        element.style.backgroundColor = 'green'
+    }
+})
