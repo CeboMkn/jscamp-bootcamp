df90f6b4fa34b82ed0037551f402c4dbf335806b
madeval <alvarez.fing@gmail.com>
2026-05-12 23:11:12 -0300

    refactorización de DetailJob usando snarkdown para parsear markdown y componente DetailJobSection reutilizable

--- 04-ejercicio-react-router-y-estado-global/src/pages/DetailJob.jsx
+++ 04-ejercicio-react-router-y-estado-global/src/pages/DetailJob.jsx
@@ -4,10 +4,12 @@ import { Link } from "../hooks/router/Link"
 import { Spinner } from "../components/Spinner"
 import ErrorPage from "./ErrorPage.jsx"
 import { useFetchDetails } from "../hooks/detailJob/useFetchDetails.js"
-import { useParseJob } from "../hooks/detailJob/useParseJob.js"
+// import { useParseJob } from "../hooks/detailJob/useParseJob.js"
 import { useAuthstore } from "../components/store/Authstore.js"
 import { BtnFavorite } from "../hooks/global/BtnFavorite.jsx"
 import { AplyButton } from "../hooks/global/AplyButton"
+
+import snarkdown from "snarkdown";
 
 
 export default function DetailJob() {
@@ -17,7 +19,8 @@ export default function DetailJob() {
     const { jobId } = useParams()
 
     const { job, loading, error } = useFetchDetails(jobId)
-    const { responsibilities, requirements } = useParseJob(job)
+    // Ya no hace falta usar `useParseJob`
+    /* const { responsibilities, requirements } = useParseJob(job) */
 
     if (loading) return <Spinner position />
     if (error) return <ErrorPage codeError="notFoundJob" />
@@ -48,75 +51,22 @@ export default function DetailJob() {
                 </div>
 
                 <div className={stl.desPuesto}>
-                    <article>
-                        <h2>Descripción del puesto</h2>
-                        <p>
-                            {job.descripcion}
-                        </p>
-                    </article>
-
-                    <section>
-                        <h2>Responsabilidades</h2>
-
-                        {responsibilities.map((text, index) => (
-                            <div key={index}>
-                                <div>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="24"
-                                        height="24"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        stroke="#09f"
-                                        strokeWidth="1"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    >
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-                                        <path d="M9 12l2 2l4 -4" />
-                                    </svg>
-                                </div>
-
-                                <p>{text}</p>
-                            </div>
-                        ))}
-                    </section>
-
-                    <section>
-                        <h2>Requisitos</h2>
-
-                        {requirements.map((text, index) => (
-                            <div key={index}>
-                                <div>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="24"
-                                        height="24"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        stroke="#09f"
-                                        strokeWidth="1"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    >
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-                                        <path d="M9 12l2 2l4 -4" />
-                                    </svg>
-                                </div>
-
-                                <p>{text}</p>
-                            </div>
-                        ))}
-                    </section>
-
-                    <article>
-                        <h2>Acerca de la empresa</h2>
-                        <p>
-                            {job.content.about}
-                        </p>
-                    </article>
+                    <DetailJobSection
+                        title="Descripción"
+                        content={job.content.description}
+                    />
+                    <DetailJobSection
+                        title="Responsabilidades"
+                        content={job.content.responsibilities}
+                    />
+                    <DetailJobSection
+                        title="Requisitos"
+                        content={job.content.requirements}
+                    />
+                    <DetailJobSection
+                        title="Acerca de la empresa"
+                        content={job.content.about}
+                    />
                 </div>
 
                 <footer className={stl.btnFooter}>
@@ -127,4 +77,19 @@ export default function DetailJob() {
             </main>
         </>
     )
+}
+
+function DetailJobSection({ title, content }) {
+  const htmlContent = snarkdown(content);
+
+  return (
+    /* Luego aquí podes poner estilos a nivel de `section` para estilar lo que quieras dentro del HTML devuelto por jobs */
+    <section>
+      <h2>{title}</h2>
+      <div
+        className={`prose`}
+        dangerouslySetInnerHTML={{ __html: htmlContent }}
+      />
+    </section>
+  );
 }
